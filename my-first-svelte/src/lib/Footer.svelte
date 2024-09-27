b563d19ae1a4ff71cee520c9ae05b514e5e6dd15
Mustafa Habibullah (1000126753) <HABIM3@student.op.ac.nz>
2024-09-27 14:39:57 +1200

    Added the image export

---
+++ my-first-svelte/src/lib/Footer.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let year = 2024;
+    export let name = 'bird';
+    export let thisismypropname;
+    let date = new Date();
+</script>
+
+<div>
+    <img src={thisismypropname} alt={name}> 
+<p>&copy; Web 1 {year} : {date}</p>
+</div>
+<style>
+    p{
+        color: chartreuse;
+        padding: 2em;
+        font-weight: bold;
+        font-size: large;
+        border: 2px dotted dodgerblue;
+    }
+</style>
--- my-first-svelte/src/routes/+page.svelte
+++ my-first-svelte/src/routes/+page.svelte
@@ -1,6 +1,18 @@
+<script>
+    import Footer from "$lib/Footer.svelte";
+    import picture from '$lib/pic/bird-8788491_1280.jpg';
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <div>
     <h2>Hello, svelte!</h2>
 </div>
+
+<Footer thisismypropname={picture} />
+
+<style>
+    :global(*){
+        font-family: cursive;
+    }
+</style>
