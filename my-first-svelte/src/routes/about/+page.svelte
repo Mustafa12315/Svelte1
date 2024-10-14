b9314643ac1710b0bde031919617ee9cdfce4ffc
Mustafa12315 <HABIM3@student.op.ac.nz>
2024-10-15 11:18:06 +1300

    Added About page

---
+++ my-first-svelte/src/routes/about/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<html>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure velit eos, alias corporis ipsam expedita accusamus distinctio, eligendi ducimus suscipit officia in, nostrum beatae officiis optio? Natus suscipit perferendis aperiam.</p>
+</html>
+
+<style>
+
+</style>
