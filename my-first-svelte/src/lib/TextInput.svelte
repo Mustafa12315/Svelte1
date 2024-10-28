0eeb1fbd17bf2ebc16ea36c75fa33f0b31989397
Mustafa12315 <HABIM3@student.op.ac.nz>
2024-10-29 11:01:22 +1300

    Binding the value input value attribute

---
+++ my-first-svelte/src/lib/TextInput.svelte
@@ -0,0 +1,6 @@
+<script>
+    let myText = "HW";
+
+</script>
+<p>{myText}</p>
+<input type="text" bind:value={myText}>
--- my-first-svelte/src/routes/+page.svelte
+++ my-first-svelte/src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
     import Header from "$lib/Header.svelte";
     import Counter from "../lib/Counter.svelte";
+    import TextInput from "../lib/TextInput.svelte";
 </script>
 
 
 <Header headingTitle="Home Page" />
 <p>This is the Homepage of the Website that you are looking at....</p>
 <Counter />
+<TextInput />
