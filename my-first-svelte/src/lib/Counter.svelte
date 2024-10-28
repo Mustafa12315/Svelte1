504554ce2bc97093d502401bc7934efa52982cc2
Mustafa12315 <HABIM3@student.op.ac.nz>
2024-10-29 10:51:09 +1300

    Added the event handler example

---
+++ my-first-svelte/src/lib/Counter.svelte
@@ -0,0 +1,11 @@
+<script>
+    let count = 0 ;
+    function incrementCount(){
+        count++;
+    }
+
+</script>
+<p>
+    The number of Count is : {count}
+</p>
+<button on:click={incrementCount}> +1 </button>
--- my-first-svelte/src/routes/+page.svelte
+++ my-first-svelte/src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
     import Header from "$lib/Header.svelte";
+    import Counter from "../lib/Counter.svelte";
 </script>
 
 
 <Header headingTitle="Home Page" />
 <p>This is the Homepage of the Website that you are looking at....</p>
+<Counter />
